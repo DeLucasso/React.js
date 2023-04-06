b27a18d7f96d29a21da86ce35ad09e552f8e2119
Lukas Nachtigall <dumdumcz@gmail.com>
2023-04-05 23:53:06 -0700

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import Message from './Message';
+
+function App() {
+// Always close the component! <Message></Message> or /
+  return <div><Message/ ></div>
+}
+
+export default App;
---
+++ src/Message.tsx
@@ -0,0 +1,9 @@
+
+// PascalCase
+function Message () {
+// JSX - JavaScript XML
+    return <h1>Hello World</h1>;
+}
+
+// We have to export the component, so it can be used somewhere else.
+export default Message;
